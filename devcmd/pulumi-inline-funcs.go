edb2cdf96f70d773612fb770181c3612cd167de5
Ameer Deen <writeameer@gmail.com>
2022-12-23 18:10:58 +0400

    Removed unsed funcs

--- devcmd/pulumi-inline-funcs.go
+++ devcmd/pulumi-inline-funcs.go
@@ -1,11 +1,6 @@
 package devcmd
 
 import (
-	"fmt"
-	"os"
-	"strings"
-
-	"github.com/dapr/cli/pkg/print"
 	pulumirunner "github.com/katasec/pulumi-runner"
 	utils "github.com/katasec/pulumi-runner/utils"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -30,68 +25,4 @@ func (d *DevCmd) createInlineProgram(pulumiFunc pulumi.RunFunc, stackName string
 	}
 
 	return pulumirunner.NewInlineProgram(args)
-}
-
-func (d *DevCmd) runWithProgressBar(message string, pulumiFunc pulumi.RunFunc, stackName string, action string) error {
-
-	var err error
-
-	// Show status
-	//stopSpinning := print.Spinner(os.Stdout, message)
-
-	// Initializer an in-line pulumi program with the passed in func
-	p := d.createInlineProgram(pulumiFunc, stackName)
-
-	// Call the appropriate action on the program
-	action = strings.ToLower(action)
-	switch action {
-	case "preview":
-		err = p.Preview()
-	case "up":
-		err = p.Up()
-	case "destroy":
-		err = p.Destroy()
-	}
-
-	// Output graphic status
-	// if err != nil {
-	// 	stopSpinning(print.Failure)
-	// } else {
-	// 	stopSpinning(print.Success)
-	// }
-
-	return err
-}
-
-func (d *DevCmd) createLocal() error {
-	runSuccess := true
-
-	// // Setup Ark Resource Group
-	// err := d.runWithProgressBar("Setup Azure resource group", createRgFunc, "dev", "up")
-	// if err != nil {
-	// 	runSuccess = false
-	// }
-	// // Setup Ark  Storage Account
-	// err = d.runWithProgressBar("Setup Azure storage account", addStrgFunc, "dev", "up")
-	// if err != nil {
-	// 	runSuccess = false
-	// }
-	// // Setup Ark Service Bus Name Space
-	// err = d.runWithProgressBar("Setup Azure Service Bus Namespace", addSbNsFunc, "dev", "up")
-	// if err != nil {
-	// 	runSuccess = false
-	// }
-
-	// Setup Ark Service Bus Name Space
-	err := d.runWithProgressBar("Setup Azure command queue", setupAzureComponents, "dev", "up")
-	if err != nil {
-		runSuccess = false
-	}
-
-	if !runSuccess {
-		fmt.Println()
-		print.InfoStatusEvent(os.Stdout, "One or more of the above had errors. Please check ark logs.")
-	}
-
-	return err
 }
