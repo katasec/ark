907e16cd8c3d40d16d2428b83ddc45de783936da
Ameer Deen <writeameer@gmail.com>
2022-12-27 17:13:34 +0400

    Fixed some bugs in the exec ShellCmd package

--- shell/shell.go
+++ shell/shell.go
@@ -3,7 +3,6 @@ package shell
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"os/exec"
 	"strings"
 )
@@ -19,7 +18,7 @@ func ExecShellCmd(shellCmdStr string) (output string, err error) {
 		cmd = exec.Command(shellCmd[0], shellCmd[1:]...)
 	} else if len(shellCmd) == 1 {
 		// shell cmd has no args
-		cmd = exec.Command(shellCmd[0], shellCmd[1:]...)
+		cmd = exec.Command(shellCmd[0])
 	} else if len(shellCmd) == 0 {
 		err = errors.New("no shell command was received")
 		output = ""
@@ -40,36 +39,4 @@ func ExecShellCmd(shellCmdStr string) (output string, err error) {
 		return out.String(), nil
 	}
 
-}
-
-func ExecShellCmd2(shellCmdStr string) error {
-
-	shellCmd := strings.Split(shellCmdStr, " ")
-
-	var cmd *exec.Cmd
-
-	if len(shellCmd) > 1 {
-		// shell cmd has arguments
-		cmd = exec.Command(shellCmd[0], shellCmd[1:]...)
-	} else if len(shellCmd) == 1 {
-		// shell cmd has no args
-		cmd = exec.Command(shellCmd[0], shellCmd[1:]...)
-	} else if len(shellCmd) == 0 {
-		err := errors.New("no shell command was received")
-		return err
-	}
-
-	var out bytes.Buffer
-	var errOut bytes.Buffer
-
-	cmd.Stdout = &out
-	cmd.Stderr = &errOut
-
-	err := cmd.Run()
-	fmt.Println(out.String())
-
-	if err != nil {
-		return errors.New(errOut.String())
-	}
-	return nil
 }
