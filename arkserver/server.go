b11ebb8601703990dbd11ee01978b2baf6fb43a1
Ameer Deen <writeameer@gmail.com>
2022-12-27 19:07:09 +0400

    testing different routers

--- arkserver/server.go
+++ arkserver/server.go
@@ -8,12 +8,6 @@ import (
 )
 
 func Start() {
-	// r := chi.NewRouter()
-	// r.Use(middleware.Logger)
-	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Write([]byte("welcome"))
-	// })
-	// http.ListenAndServe(":3000", r)
 
 	r := arkrouter.NewChiRouter()
 
