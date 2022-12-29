1ad5abdf5e579aa2bf1c7264ef871c83ef37d7b7
Ameer Deen <writeameer@gmail.com>
2022-12-29 13:04:52 +0400

    Setup routes for server

--- server/routes.go
+++ server/routes.go
@@ -1,17 +1,8 @@
 package server
 
-import "net/http"
-
 func (s *Server) initaliseRoutes() {
 
 	// Register route handlers for routes
 	s.router.GET("/", s.homeHander())
 
-	s.azureGet("home", s.homeHander())
-}
-
-// azureRoute registers an Azure Route with the router
-func (s *Server) azureGet(uri string, f http.HandlerFunc) {
-	// Register route handlers for routes
-	s.router.GET("/azure/"+uri, s.homeHander())
 }
