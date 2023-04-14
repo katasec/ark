6b3c8842e5c80d7222b6cfb87cad9494aa62ce1d
Ameer Deen <ameer.deen@qigroup.com>
2023-04-14 16:33:46 +0400

    Added Update/Delete Cloudspace

--- dbtest/dbtest.go
+++ dbtest/dbtest.go
@@ -33,26 +33,6 @@ func DbStuff() {
 	repo.UpdateCloudSpace(acs)
 	repo.DeleteCloudSpace(acs)
 }
-func CreateTable() {
-
-	db := OpenDb()
-	defer db.Close()
-
-	// Create table
-	sql_table := `
-	CREATE TABLE IF NOT EXISTS cloudspaces(
-		id integer NOT NULL PRIMARY KEY,
-		name text,
-		cloudspace text
-	);
-	`
-	_, err := db.Exec(sql_table)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-//NewAzureCloudSpaceRepository
 
 func OpenDb() *sql.DB {
 	db, err := sql.Open("sqlite3", "./test.db")
