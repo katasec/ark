13f7a4c9b30767efd4ed72ab13b3dbc7457c4005
Ameer Deen <writeameer@gmail.com>
2023-11-20 16:16:19 +0400

    Restructuring folders for handlers

---
+++ arkserver/arkserver.go
@@ -0,0 +1,20 @@
+package arkserver
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/katasec/ark/messaging"
+	"github.com/katasec/ark/repositories"
+)
+
+type Server interface {
+	GetQClient() messaging.Messenger
+	GetAcsrepo() *repositories.AzureCloudSpaceRepository
+	GetRouter() *chi.Mux
+}
+
+// type ServerInterface interface {
+// 	GetQClient() messaging.Messenger
+// 	GetRouter() *chi.Mux
+// 	GetDb() *sql.DB
+// 	GetAcsrepo() *repositories.AzureCloudSpaceRepository
+// }
---
+++ server/handlers/cloudspace.go
@@ -0,0 +1,93 @@
+package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/katasec/ark/arkserver"
+	"github.com/katasec/ark/requests"
+	"github.com/katasec/ark/resources/v0/azure/cloudspaces"
+	"gopkg.in/yaml.v2"
+)
+
+func PostCloudspace(s arkserver.Server) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// Deference pointer to get acsrepo and qclient
+		acsRepo := s.GetAcsrepo()
+		qClient := s.GetQClient()
+
+		// Set default acs name
+		acsRequest := requests.CreateAzureCloudspaceRequest{
+			Name: "default",
+		}
+
+		// Decode request body into acsRequest
+		err := yaml.NewDecoder(r.Body).Decode(&acsRequest)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// Get cloudspace from DB
+		acs, err := acsRepo.GetCloudSpace(acsRequest.Name)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Error getting cloudspace: %s,", err)
+			return
+		}
+
+		// Generate new cloudspace struct if none found in DB
+		if acs.Name == "" {
+			fmt.Println("Cloudspace not found in DB, creating new cloudspace")
+			acs = *(cloudspaces.NewAzureCloudSpace())
+			fmt.Println(acs.ToJson())
+		}
+
+		// Add environments from reuqest into struct
+		for _, env := range acsRequest.Environments {
+			acs.AddSpoke(env)
+		}
+		fmt.Println("The hub's name:", acs.Hub.Name)
+
+		// Send request to queue
+		err = qClient.Send("azurecloudspace", acs.ToJson())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Internal Error: %s,", err)
+			return
+		} else {
+			log.Println("Message sent successfully !")
+		}
+
+		// Save cloudspace to DB
+		acsRepo.AddCloudSpace(&acs)
+		w.Header().Set("Content-Type", "application/x-yaml")
+		w.WriteHeader(http.StatusOK)
+	})
+}
+
+func DeleteCloudspace(s arkserver.Server) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		request := requests.DeleteAzureCloudspaceRequest{
+			Name: "default",
+		}
+
+		err := yaml.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/x-yaml")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprint(w, request.ToYamlAzureCloudpace())
+
+		qClient := s.GetQClient()
+		qClient.Send("DeleteAzureCloudspaceRequest", request.ToJsonAzureCloudpace())
+
+	})
+}
