9104293fda1a20665d9335880da02e0f4efcfe03
Ameer Deen <writeameer@gmail.com>
2024-01-03 21:37:06 +0400

    Updated local path used by tfrunner

--- tfrunner/tfrunner_test.go
+++ tfrunner/tfrunner_test.go
@@ -13,8 +13,6 @@ func TestRunHello(t *testing.T) {
 	acs.AddSpoke("dev")
 	acs.AddSpoke("prod")
 
-	//fmt.Println(acs.ToJson())
-
 	fmt.Println("Testing image download")
 	runner := NewTfrunner("ark-resource-hello:v0.0.1", acs.ToJson())
 	runner.Run()
